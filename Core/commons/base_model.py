2db64a756b298b30dbcfd7e3678dd6b9985f1060
denissragalj-private <denis.sragalj@gmail.com>
2025-10-27 20:15:54 +0100

    Update base_model.py

--- Core/commons/base_model.py
+++ Core/commons/base_model.py
@@ -5,28 +5,20 @@ import datetime as dt
 class BaseModel:
     """Base class for all models in the application."""
     def __init__(self, 
-                 id: int, 
                  name: str,
-                 description: str ="",
-                 created_at: str = "",
-                 created_by: str = "",
-                 updated_at: str = "",
-                 updated_by: str = "",
-                 deleted_at: str = "",
-                 deleted_by: str = "",
-                 # Soft delete flag
-                 is_deleted: bool = False):
-        
-        self.id = id
+                 description: str =""):
         self.name = name
         self.description = description
-        self.created_at = created_at
-        self.created_by = created_by
-        self.updated_at = updated_at
-        self.updated_by = updated_by
-        self.deleted_at = deleted_at
-        self.deleted_by = deleted_by
+        self.id = 1
+        self.tenant_id = '' # placeholder for tenant identifier uuid
+        self.created_at = dt.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.created_by = ''
+        self.updated_at = dt.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.updated_by = ''
+        self.deleted_at = dt(1900, 1, 1).strftime("%Y-%m-%d %H:%M:%S"
+        self.deleted_by = ''
         self.is_deleted = is_deleted
+
 
 
     def __repr__(self):
