466433847c702651e43e76e3989b3ba42660130f
denissragalj-private <denis.sragalj@gmail.com>
2025-10-27 20:34:08 +0100

    all

--- Core/commons/base_model.py
+++ Core/commons/base_model.py
@@ -1,33 +1,22 @@
-import datetime as dt
-
+from datetime import datetime as dt
 
 
 class BaseModel:
     """Base class for all models in the application."""
-    def __init__(self, 
+    def __init__(self,
                  name: str,
-                 description: str =""):
+                 description: str = ""):
         self.name = name
         self.description = description
         self.id = 1
-        self.tenant_id = '' # placeholder for tenant identifier uuid
+        self.tenant_id = '' # Placeholder for tenant identifier uuid
         self.created_at = dt.now().strftime("%Y-%m-%d %H:%M:%S")
         self.created_by = ''
-        self.updated_at = dt.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.updated_at = dt(1900, 1, 1, 0, 0, 0).strftime("%Y-%m-%d %H:%M:%S")
         self.updated_by = ''
-        self.deleted_at = dt(1900, 1, 1).strftime("%Y-%m-%d %H:%M:%S"
+        self.deleted_at = dt(1900, 1, 1, 0, 0, 0).strftime("%Y-%m-%d %H:%M:%S")
         self.deleted_by = ''
-        self.is_deleted = is_deleted
-
-
+        self.is_deleted = False
 
     def __repr__(self):
         return f"Name '{self.name}', Description '{self.description}'"
-
-    def __str__(self):
-        return f"Name '{self.name}'"
-    
-    def delete(self, deleted_by: str)
-        self.is_deleted = True
-        self.deleted_at = dt.now().strftime("%Y-%m-%d %H:%M:%S")        
-        self.deleted_by = deleted_by
