2af8f8b211af657bb0e385c0362dc26880692240
Denis Šragalj <denis.sragalj@gmail.com>
2025-10-28 08:46:47 +0100

    end

--- Core/guitars/guitar_types.py
+++ Core/guitars/guitar_types.py
@@ -7,23 +7,3 @@ class GuitarType(BaseModel):
 
     def __repr__(self):
         return f"Guitar type '{self.name}'"
-
-
-# ACOUSTIC = "acoustic"
-# ELECTRIC = "electric"
-# BASS = "bass"
-# CLASSICAL = "classical"
-# RESONATOR = "resonator"
-# TWELVE_STRING = "12_string"
-# SEVEN_STRING = "7_string"
-# NINE_STRING = "9_string"
-# BARITONE = "baritone"
-# MIDI = "midi"
-# FRETLESS = "fretless"
-# PARLOR = "parlor"
-# DREADNOUGHT = "dreadnought"
-# JUMBO = "jumbo"
-# CONCERT = "concert"
-# AUDITORIUM = "auditorium"
-# TRAVEL = "travel"
-# PARLOR = "parlor"
